9024199990fe10be2fe793e0caaf276ed18b4686
hasan <hassanhitleap1@gmail.com>
2018-07-05 15:20:47 +0300

    generate code app

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'dsfccsdvsfdfvf5sd51sd51sdSDXZZ@#!@!#$',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
